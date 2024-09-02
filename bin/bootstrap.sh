777cbc98b0a53355892f9fe29541379cd0882540
Sérgio Bernardino <me@sergiobernardino.net>
2024-09-02 22:31:09 +0100

    Added support for a 'config' sub-dir.

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -3,26 +3,39 @@
 SCRIPT_DIR=`dirname $(readlink -f $0)`
 ROOT_DIR=`dirname $SCRIPT_DIR`
 
-for FILE in "$ROOT_DIR"/*
-do
-  if [[ $SCRIPT_DIR == $FILE ]]
+function create_links() {
+  HIDE_TARGET=true
+  TARGET_DIR="$HOME"
+  SOURCE_DIR="$ROOT_DIR"
+
+  if [[ ! -z $1 ]]
   then
-    continue
+    HIDE_TARGET=false
+    TARGET_DIR="$HOME/.$1"
+    SOURCE_DIR="$ROOT_DIR"/"$1"
   fi
 
-  FILENAME=$(basename "$FILE")
-  TARGET="$HOME/.$FILENAME"
+  for FILE in "$SOURCE_DIR"/*
+  do
+    if [[ $FILE = $SCRIPT_DIR || $FILE == *.md || $FILE == *config ]]
+    then
+      continue
+    fi
 
-  if [[ ! -e $TARGET ]]
-  then
-    if [[ $TARGET != *.md ]]
+    FILENAME=$(basename "$FILE")
+    TARGET=$([[ $HIDE_TARGET == true ]] && echo "$TARGET_DIR/.$FILENAME" || echo "$TARGET_DIR/$FILENAME")
+
+    if [[ ! -e $TARGET ]]
     then
       echo "Creating a symlink of '$FILE' to '$TARGET'."
-      # ln -s "$FILE" "$TARGET"
+      ln -s "$FILE" "$TARGET"
+    else
+      echo "SKIPPED: '$TARGET' already exists!"
     fi
-  else
-    echo "SKIPPED: '$TARGET' already exists!"
-  fi
-done
+  done
+}
+
+create_links
+create_links 'config'
 
 echo "Done."
